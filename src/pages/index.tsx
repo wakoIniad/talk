1804176c134aa6b81364d3d93efe90053f601935
wakoIniad <s1f102400606@iniad.org>
2024-09-17 07:11:39 +0900

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,12 @@ import { ButtonLayers, ButtonElement } from './layer-ui'
 
 const UI_BORDER_WEIGHT = 20;//px
 let uiDivisionCount = 10;
+const UI_DIVISION_COUNT_EACH_LAYER = [ 1, 5, 5 ];
 
 
-function uiClicked(args: {id: number, type: string}) {
-  const {id, type} = args;
-  console.log(id,type)
+function uiClicked(args: {id: number, layer: number}) {
+  const {id, layer} = args;
+  console.log(id,layer)
 }
 
 const layer = new ButtonLayers(['', ...'kstnhmyrw'.split('')]
@@ -27,60 +28,65 @@ console.log(layer)
 const Home = () => {
   const { width, height } = getWindowSize();
   const vmin = Math.min(width, height);
-  const zeroOneScaleBorderWeight = 1/vmin*1*UI_BORDER_WEIGHT;
+
+  function makeButton( layer:number, i:number, size:number ) {
+    const reScaledBorderWeight = 1/vmin*1*UI_BORDER_WEIGHT * size
+    const DIVISION_COUNT = UI_DIVISION_COUNT_EACH_LAYER[layer];
+    const button =
+      <button
+        className={`${styles.input_ui_btn} ${styles[`input_ui_btn_${layer}`]} ${styles[`input_ui_btn_${layer}_${i}`]}`}
+        onClick={()=>uiClicked({ layer:layer, id:i })}
+        style={{
+          clipPath: `url(#btn_clip_${layer}_${i})`,
+          width: `${100*size}%`,
+          height: `${100*size}%`
+        }}
+      ></button>;
+
+      const getPos = (
+        f:(rad: number) => number,
+        i:number
+      ) => f(2*Math.PI/DIVISION_COUNT*i)/2;
+
+      const [ ax, ay, bx, by, ] =
+        [ { method: Math.cos, index: i },
+          { method: Math.sin, index: i },
+          { method: Math.cos, index: i + 1 },
+          { method: Math.sin, index: i + 1 },
+        ].map( ({method, index}: {
+          method: (rad:number)=>number,
+          index: number
+        }) => getPos(method, index) );
+      const mx = (ax + bx)/2 * reScaledBorderWeight + 0.5;
+      const my = (ay + by)/2 * reScaledBorderWeight + 0.5;
+
+      const svg =
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <clipPath id={`btn_clip_${layer}_${i}`} clipPathUnits="objectBoundingBox">
+            <path d={`M ${mx} ${my} l ${ax} ${ay} a 0.5 0.5 0 ${size >= 2 ? 0 : 1} 1 ${bx - ax} ${by - ay} Z`} fill="none"/>
+          </clipPath>
+        </svg>;
+      return { button, svg };
+  }
   return (
     <div className={styles.container}>
       <div className={styles.input_ui_container}>
         {
           (function(){
+            const centerBtn = makeButton(0, 0, 0.5);
             const buttons = [
-              <button
-                className={`${styles.input_ui_btn} ${styles.input_ui_btn_center}`}
-                onClick={()=>uiClicked({ type:"center", id:uiDivisionCount })}
-              ></button>
+              centerBtn.button,
             ];
 
             const svgs = [
-
+              centerBtn.svg,
             ];
-            for(let i = 0;i < uiDivisionCount;i++) {
-              buttons.push(
-              <button
-                className={`${styles.input_ui_btn} ${styles[`input_ui_btn_circumference_${i}`]}`}
-                onClick={()=>uiClicked({ type:"ring", id:i })}
-                style={{ clipPath: `url(#btn_clip_${i})` }}
-              ></button>);
 
-              const getPos = (
-                f:(rad: number) => number,
-                i:number
-              ) => f(2*Math.PI/uiDivisionCount*i)/2;
-
-              const [ ax, ay, bx, by, ] =
-                [ { method: Math.cos, index: i },
-                  { method: Math.sin, index: i },
-                  { method: Math.cos, index: i + 1 },
-                  { method: Math.sin, index: i + 1 },
-                ].map( ({method, index}: {
-                  method: (rad:number)=>number,
-                  index: number
-                }) => getPos(method, index) );
-              const mx = (ax + bx)/2 * zeroOneScaleBorderWeight + 0.5;
-              const my = (ay + by)/2 * zeroOneScaleBorderWeight + 0.5;
-
-              //const [ ax, ax_raw ] = getPos(Math.cos, i);
-              //const [ ay, ay_raw ] = getPos(Math.sin, i);
-              //const [ bx, bx_raw ] = getPos(Math.cos, i+1);
-              //const [ by, by_raw ] = getPos(Math.sin, i+1);
-
-              svgs.push(
-                <svg xmlns="http://www.w3.org/2000/svg">
-                  <clipPath id={`btn_clip_${i}`} clipPathUnits="objectBoundingBox">
-                    <path d={`M ${mx} ${my} l ${ax} ${ay} a 0.5 0.5 0 0 1 ${bx - ax} ${by - ay} Z`} fill="none"/>
-                  </clipPath>
-                </svg>
-              );
-            }
+            /*for(let i = 0;i < uiDivisionCount;i++) {
+              const { button, svg } = makeButton(i,1,0.8);
+              buttons.push(button);
+              svgs.push(svg);
+            }*/
             return [...svgs,...buttons];
           })()
         }
